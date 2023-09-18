b7bcff0060e225832b5554f775047516580569b6
tesddev <tesleem.amuda@gmail.com>
2023-09-18 13:35:04 +0100

    present alert showing to user the correctness of their guess

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var isShowingScore = false
+    @State private var scoreTitle = ""
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
     var body: some View {
         ZStack {
             Color.blue
@@ -23,14 +25,33 @@ struct ContentView: View {
                 }
                 ForEach(0..<3) { number in
                     Button{
-                        
+                        flagTapped(number)
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
                     }
+                    .alert(scoreTitle, isPresented: $isShowingScore) {
+                        Button("Continue", action: askQuestion)
+                    } message: {
+                        Text("Your score is ")
+                    }
                 }
             }
         }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        isShowingScore = true
+    }
+    
+    func askQuestion(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
