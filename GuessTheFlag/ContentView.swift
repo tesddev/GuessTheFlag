f286af132fad639997cda7aef22946593c80fcc1
tesddev <tesleem.amuda@gmail.com>
2023-09-18 16:02:50 +0100

    use linear gradient and format instruction label text

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -14,14 +14,16 @@ struct ContentView: View {
     @State private var correctAnswer = Int.random(in: 0...2)
     var body: some View {
         ZStack {
-            Color.blue
+            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             VStack(spacing: 30) {
                 VStack {
                     Text("Tap the flag of")
                         .foregroundColor(.white)
+                        .font(.subheadline.weight(.heavy))
                     Text(countries[correctAnswer])
                         .foregroundColor(.white)
+                        .font(.largeTitle.weight(.semibold))
                 }
                 ForEach(0..<3) { number in
                     Button{
